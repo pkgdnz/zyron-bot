27ab579710e1a71d9adccb1a233fef6b0298ec8a
pkgdnz <dnz.web.id@gmail.com>
2026-08-15 21:15:01 +0800

    cleanup: simplify ping plugin

--- src/plugins/ping.js
+++ src/plugins/ping.js
@@ -1,16 +1,14 @@
-const run = async ctx => {
-   const { jid, sock } = ctx;
-   const start = Date.now();
-
-   const result = await sock.message.send(jid, 'pong!');
+const run = async ({ jid, sock }) => {
+    await sock.message.send(jid, 'pong!');
 };
 
 const plugin = {
-   run,
-   name: 'ping',
-   command: ['ping'],
-   description: 'Respons dengan pengecekan latensi.',
-   category: ['core']
+    run,
+    name: 'ping',
+    command: ['ping'],
+    ownerOnly: false,
+    description: 'Check bot response and latency.',
+    category: ['core']
 };
 
 export default plugin;
