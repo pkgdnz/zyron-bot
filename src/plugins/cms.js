0dea99f7e11e0549d314558710dbba4712e2cd1e
pkgdnz <dnz.web.id@gmail.com>
2026-08-16 23:08:54 +0800

    refactor(plugins): make cms owner only

--- src/plugins/cms.js
+++ src/plugins/cms.js
@@ -5,7 +5,7 @@ import { join } from 'node:path';
 
 import { resolveMessageContent } from '../message-resolve.js';
 
-const IDENT_RE = /^[A-Za-z_$][A-Za-z0-9_$]*$/;
+const IDENT_RE = /^[A-Za-z_$][A-Za-z0-9_]*$/;
 
 const indent = (str, spaces) => {
     const pad = ' '.repeat(spaces);
@@ -103,6 +103,7 @@ const plugin = {
     name: 'cms',
     command: ['cms'],
     description: 'Menghasilkan kode reproduksi untuk pesan yang di-quote.',
+    ownerOnly: true,
     category: ['core']
 };
 
